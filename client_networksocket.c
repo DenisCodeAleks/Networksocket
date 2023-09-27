ca6b6866ffbc98594a41f873bb20f72e774ebbe3
DenisCodeAleks <114106833+DenisCodeAleks@users.noreply.github.com>
2023-09-28 01:10:22 +0200

    Update client_networksocket.c
    final version

--- client_networksocket.c
+++ client_networksocket.c
@@ -1,37 +1,79 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-//Headerdateien alte Version BSD
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> //Schnittstellen UNIX Standard Bibliothek
-#include <netinet/in.h>
-#include <netdb.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include <stdio.h> 
+    
+    int client_desc;
+    struct sockaddr_in server_address;
+    char buffer_to_server[1024], buffer_to_client[1024];
+    int checksum = 0; 
 
+    int createsocket ();
+    void connectsocket ();
+    int communicate ();
+    void closesocket();
 
 int main() {
-    int client_socket;
-    struct sockaddr_in server_address;
-    char buffer[1024];
 
-    // Socket erstellen
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    createsocket();
 
-    // Server-Adresse konfigurieren
+    //Serveradresse konfigurieren
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(8080);
-    server_address.sin_addr.s_addr = inet_addr("192.168.36.90"); 
-    // Mit Server verbinden
-    connect(client_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+    server_address.sin_addr.s_addr = inet_addr("192.168.189.119");  //IP vom Server
 
-    // Daten senden und empfangen
-    send(client_socket, "Hello from Client!", sizeof("Hello from Client!"), 0);
-    recv(client_socket, buffer, sizeof(buffer), 0);
-    printf("Client received: %s\n", buffer);
+    connectsocket();
+  
+    if (checksum != -1)
+    {
+    communicate();
 
-    // Socket schließen
-    close(client_socket);
 
+    closesocket();
+    }
     return 0;
 }
+
+int createsocket()
+{
+    client_desc = socket(AF_INET, SOCK_STREAM, 0);
+}
+void connectsocket()
+{
+    if(connect(client_desc, (struct sockaddr*) &server_address, sizeof(server_address)) < 0)
+    {
+        printf("Verbindung zu Server nicht möglich");
+        checksum = -1;
+    }
+}
+
+int communicate()
+{
+    while(1) {
+        printf("Bitte Nachricht zum Versenden eingeben: ");
+        fgets(buffer_to_server, sizeof(buffer_to_server), stdin);     
+        send(client_desc, buffer_to_server, strlen(buffer_to_server), 0);
+
+        if (strcmp(buffer_to_server, "exit\n") == 0)
+        {
+            break;
+        }
+        printf("Message has been sent. Please wait...\n");
+
+        memset(buffer_to_client, 0, sizeof(buffer_to_client));
+        recv(client_desc, buffer_to_client, sizeof(buffer_to_client), 0);
+        if ((strcmp(buffer_to_client, "kick\n") == 0) || (strcmp(buffer_to_client, "") ==0 ))
+        {
+            printf("Keine Verbindung zum Server");
+            break;
+        }
+        printf("Client hat folgende Nachricht erhalten: %s", buffer_to_client);
+    }
+}
+
+void closesocket()
+{
+    close(client_desc);
+}
+
